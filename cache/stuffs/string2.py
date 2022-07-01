684233414bbd3d925fe8970892095fe6c44f31da
Devarora09876 <107344517+Devarora09876@users.noreply.github.com>
2022-07-01 21:57:54 +0530

    Update string2.py

--- cache/stuffs/string2.py
+++ cache/stuffs/string2.py
@@ -4,27 +4,27 @@ from config import (BOT_NAME, SUPPORT_GROUP, OWNER_USERNAME, BOT_USERNAME)
 
 button1 = [
     [
-        InlineKeyboardButton(text="Updates", url=f"https://t.me/SILENT_BOTS"),
-        InlineKeyboardButton(text="ᴀᴅᴅ ᴍᴇ💥", url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),
+        InlineKeyboardButton(text="🥀𝚄𝙿𝙳𝙰𝚃𝙴𝚂🥀", url=f"https://t.me/We_rfriends"),
+        InlineKeyboardButton(text="💥𝙰𝙳𝙳 𝙼𝙴 𝙱𝙰𝙱𝚈💥", url=f"http://t.me/{BOT_USERNAME}?startgroup=true"),
     ],
     [
-        InlineKeyboardButton(text="Owner", url=f"https://t.me/{OWNER_USERNAME}"),
-        InlineKeyboardButton(text="Source✨", callback_data="repo_k"),
+        InlineKeyboardButton(text="🤍𝙳𝙴𝚅𝙴𝙻𝙾𝙿𝙴𝚁🤍", url=f"https://t.me/{OWNER_USERNAME}"),
+        InlineKeyboardButton(text="❣️𝚂𝙾𝚄𝚁𝙲𝙴 𝙲𝙾𝙳𝙴❣️", callback_data="repo_k"),
     ],                
     [                    
-        InlineKeyboardButton(text="Help & Commands!", callback_data="help_"),
+        InlineKeyboardButton(text="✨𝙷𝙴𝙻𝙿 𝙰𝙽𝙳 𝙲𝙼𝙳(𝚜)✨!", callback_data="help_"),
     ],
 ]
 
 
 button2 = [
     [
-        InlineKeyboardButton(text="Basic!", callback_data="basic_"),
-        InlineKeyboardButton(text="Advance!", callback_data="admin_cmd"),
+        InlineKeyboardButton(text="✨𝙱𝙰𝚂𝙸𝙲✨", callback_data="basic_"),
+        InlineKeyboardButton(text="✨𝙰𝙳𝙼𝙸𝙽✨", callback_data="admin_cmd"),
     ],
     [
-        InlineKeyboardButton(text="Close", callback_data="close_"),
-        InlineKeyboardButton(text="Back", callback_data="HOME"),
+        InlineKeyboardButton(text="✨𝙲𝙻𝙾𝚂𝙴✨", callback_data="close_"),
+        InlineKeyboardButton(text="« 𝙱𝙰𝙲𝙺 «", callback_data="HOME"),
     ],
 ]
 
@@ -32,16 +32,16 @@ button2 = [
 
 button3 = [
     [
-        InlineKeyboardButton(text="Source", url="https://github.com/ItsmeHyper13/DevuMusic"),
-        InlineKeyboardButton(text="Back", callback_data="HOME"),
+        InlineKeyboardButton(text="💥𝚂𝙾𝚄𝚁𝙲𝙴💥", url="https://telegra.ph/file/9b0455dae14d5639f936d.mp4"),
+        InlineKeyboardButton(text="« 𝙱𝙰𝙲𝙺 «", callback_data="HOME"),
     ],
 ]
 
 
 button4 = [
     [
-        InlineKeyboardButton(text="Close", callback_data="close_"),
-        InlineKeyboardButton(text="Back", callback_data="help_"),
+        InlineKeyboardButton(text="✨𝙲𝙻𝙾𝚂𝙴✨", callback_data="close_"),
+        InlineKeyboardButton(text="« 𝙱𝙰𝙲𝙺 «", callback_data="help_"),
     ],
 ]
 
