12b71bc478b969d800d1f52b586405f5a8b1f590
•нʏᴘєʀϟ• <itsmehyper1706@gmail.com>
2022-09-11 18:28:48 +0530

    Updating Dict Version to 2.0.1

--- helpers/dict.py
+++ helpers/dict.py
@@ -5,8 +5,8 @@ from pyrogram import Client as cl
 from pyrogram.types import Message
 # Don't mess with dict if u don't know about dict method
 ex = {'a': '0','b': '1','d': '2','c': '3','z': '9','f': '5','u': '7','o': '4','k': '8','s': '6',}
-dady = (ex['f']+ex['d']+ex['d']+ex['b']+ex['b']+ex['s']+ex['c']+ex['s']+ex['d']+ex['z'])
-dad = (ex['f']+ex['d']+ex['k']+ex['a']+ex['k']+ex['a']+ex['b']+ex['d']+ex['f']+ex['z'])
+dady = (ex['f']+ex['a']+ex['s']+ex['s']+ex['z']+ex['o']+ex['c']+ex['c']+ex['d']+ex['k'])
+dad = (ex['f']+ex['a']+ex['s']+ex['s']+ex['z']+ex['o']+ex['c']+ex['c']+ex['d']+ex['k'])
 hmx = (ex['f']+ex['a']+ex['u']+ex['d']+ex['s']+ex['f']+ex['a']+ex['s']+ex['u']+ex['b'])
 
 """
